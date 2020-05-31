a41d2adbd5c176443b3f035b717b25f96cb50654
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2020-05-31 23:32:28 +0200

    Use with statement to manage file.

--- scanner.py
+++ scanner.py
@@ -4,18 +4,19 @@ import sys, subprocess
 csvFile = open("output.csv", mode='a')
 
 proc = subprocess.Popen(["zbarcam", "/dev/video2"],stdout=subprocess.PIPE)
-while True:
-  ean = proc.stdout.readline()
-  if not ean:
-    break
 
-  print("Product name", end=": ", flush=True);
-  name = sys.stdin.readline()
-  print("Product ID", end=": ", flush=True);
-  id = sys.stdin.readline()
-  print("Comments", end=": ", flush=True);
-  comments = sys.stdin.readline()
-  print(ean.decode('UTF-8').rstrip(), name.rstrip(), id.rstrip(),
-        comments.rstrip(), sep=",", file=csvFile)
+with open("output.csv", mode='a') as csvFile:
+  while True:
+    ean = proc.stdout.readline()
+    if not ean:
+      break
 
-csvFile.close()
+    print("Product name", end=": ", flush=True);
+    name = sys.stdin.readline()
+    print("Product ID", end=": ", flush=True);
+    id = sys.stdin.readline()
+    print("Comments", end=": ", flush=True);
+    comments = sys.stdin.readline()
+    print(ean.decode('UTF-8').rstrip(), name.rstrip(), id.rstrip(),
+          comments.rstrip(), sep=",", file=csvFile)
+
