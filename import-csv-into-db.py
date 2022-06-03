13e18d9e33627533575ce9344f5e861d92b4e1a3
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2022-06-03 15:36:46 +0200

    Implement more advanced CLI for import-csv-into-db.

--- import-csv-into-db.py
+++ import-csv-into-db.py
@@ -1,21 +1,43 @@
 #!/usr/bin/env python
-import sys
+import getopt, sys
 import sqliteBackend
 
-##### Configuration
-databaseFile = "games.sqlite"
+def printHelpAndExit():
+  print("Usage: import-csv-into-db.py [OPTION]... [CSV FILE]...",
+      "Import rows from CSV FILEs into the SQLite database stored in DB FILE.",
+      "",
+      "Options:",
+      "  -h, --help                        Show help",
+      "  -o [DB FILE], --output=[DB FILE]  REQUIRED. Output file with SQLite database.",
+      sep="\n")
+  sys.exit()
 
 
 def main():
-  if len(sys.argv) == 1:
-    print("Error: missing CSV file argument.",
-        "Usage: import-csv-int-db.py [CSV FILE] [CSV FILE] ...",
-        "Imports rows from CSV files into a SQLite database.", sep="\n");
-    sys.exit()
+  databaseFile = ""
+  inputFiles = []
+
+  try:
+    arguments, inputFiles = getopt.getopt(sys.argv[1:], "ho:", ["help", "output="])
+    for currentArgument, currentValue in arguments:
+        if currentArgument in ("-h", "--help"):
+          printHelpAndExit()
+        elif currentArgument in ("-o", "--output"):
+          databaseFile = currentValue
+  except getopt.error as err:
+      # output error, and return with an error code
+      print ("Error:", str(err))
+      printHelpAndExit()
+
+  if databaseFile == "":
+    print("Error: missing output file argument.")
+    printHelpAndExit()
+  if len(inputFiles) == 0:
+    print("Error: missing input CSV file(s).")
+    printHelpAndExit()
 
   db = sqliteBackend.setupDatabaseFile(databaseFile)
-
-  for inputFile in sys.argv:
+  for inputFile in inputFiles:
     if "import-csv-into-db.py" in inputFile:
       continue
     sqliteBackend.importFromCSVFile(db, inputFile)
